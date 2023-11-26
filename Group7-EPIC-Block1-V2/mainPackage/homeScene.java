cb23c2382e60d7cc79377c70d3352485fb27dd39
unknown <23371323@studentmail.ul.ie>
2023-11-26 17:13:19 +0000

    homeScene Functions

--- Group7-EPIC-Block1-V2/mainPackage/homeScene.java
+++ Group7-EPIC-Block1-V2/mainPackage/homeScene.java
@@ -1,5 +1,4 @@
 package mainPackage;
-
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -17,74 +16,72 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
-
-
 public class homeScene extends Scene{
-	Background transparentBackground = new Background(new BackgroundFill(Color.TRANSPARENT, CornerRadii.EMPTY, javafx.geometry.Insets.EMPTY));
+
+     Background transparentBackground = new Background(new BackgroundFill(Color.TRANSPARENT, CornerRadii.EMPTY, javafx.geometry.Insets.EMPTY));
 	 Background orangeBackground = new Background(new BackgroundFill(Color.rgb(232, 123, 56), CornerRadii.EMPTY, javafx.geometry.Insets.EMPTY));
 	 DropShadow dropShadow = new DropShadow();
-	
-	Insets offset = new Insets(10,10,10,10);
-    Label Intro = new Label("Welcome to the quiz! Please Login or Sign up");
-    static Button loginButton = new Button("Login");
-    Button signUpButton = new Button("Sign Up");
+     Insets offset = new Insets(10,10,10,10);
 
     public homeScene(Stage primaryStage) {
         super(new VBox(), 440, 250);
-        Intro.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 13));
-        Intro.setTextFill(Color.WHITE);
-        Intro.setBackground(transparentBackground);
-        Intro.setEffect(dropShadow);
-        Intro.setTextAlignment(TextAlignment.CENTER);
-        Intro.setAlignment(Pos.CENTER);
-        
-        loginButton.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 10));
-        loginButton.setTextFill(Color.WHITE);
-        loginButton.setBackground(orangeBackground);
-        loginButton.setEffect(dropShadow);
-        loginButton.setTextAlignment(TextAlignment.CENTER);
-        loginButton.setAlignment(Pos.CENTER);
-        
-        signUpButton.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 10));
-        signUpButton.setTextFill(Color.WHITE);
-        signUpButton.setBackground(orangeBackground);
-        signUpButton.setEffect(dropShadow);
-        signUpButton.setTextAlignment(TextAlignment.CENTER);
-        signUpButton.setAlignment(Pos.CENTER);
-        
-        //home layout
-        GridPane homeLay = new GridPane();
-        signUpButton.setOnAction(e -> {
-            Stage stage = (Stage) signUpButton.getScene().getWindow();
-            primaryStage.setScene(signupScene.createScene(stage));
 
-        });
+        Label Intro = new Label("Welcome to the quiz! Please Login or Sign up");
+        labelCosmetics(Intro);
 
+        Button loginButton = new Button("Login");
+        buttonCosmetics(loginButton);
         loginButton.setOnAction(e -> {
             Stage stage = (Stage) loginButton.getScene().getWindow();
             primaryStage.setScene(loginScene.createScene(stage));
         });
 
-        //General layout settings
+        Button signUpButton = new Button("Sign Up");
+        buttonCosmetics(signUpButton);
+        signUpButton.setOnAction(e -> {
+            Stage stage = (Stage) signUpButton.getScene().getWindow();
+            primaryStage.setScene(signupScene.createScene(stage));
+        });
+
+        Button placeholder = new Button();
+
+        //layout settings------------------------------------
+        GridPane homeLay = new GridPane();
         homeLay.setStyle("-fx-background-color: #FFD966;");
         homeLay.setPadding(offset);
         homeLay.setVgap(10);
         homeLay.setHgap(5);
         homeLay.setAlignment(Pos.CENTER);
+
         //Children addition and positioning
         homeLay.setConstraints(Intro, 1, 0);
-        homeLay.setConstraints(loginButton, 0, 2);
-        homeLay.setConstraints(signUpButton, 2, 2);
-        homeLay.getChildren().addAll(Intro, loginButton, signUpButton);
-
-        // Add the button to the scene's layout
+        homeLay.setConstraints(loginButton, 0, 10);
+        homeLay.setConstraints(signUpButton, 2, 10);
+        homeLay.setConstraints(placeholder, 1, 100);
+        homeLay.getChildren().addAll(Intro, loginButton, signUpButton, placeholder);
         VBox root = (VBox) this.getRoot();
         root.getChildren().addAll(homeLay);
-
     }
+
     public static homeScene createScene(Stage primaryStage) {
         return new homeScene(primaryStage);
     }
 
+    public void labelCosmetics(Label label){
+        label.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 13));
+        label.setTextFill(Color.WHITE);
+        label.setBackground(transparentBackground);
+        label.setEffect(dropShadow);
+        label.setTextAlignment(TextAlignment.CENTER);
+        label.setAlignment(Pos.CENTER);
+    }
 
+    public void buttonCosmetics(Button button){
+        button.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 15));
+        button.setTextFill(Color.WHITE);
+        button.setBackground(orangeBackground);
+        button.setEffect(dropShadow);
+        button.setTextAlignment(TextAlignment.CENTER);
+        button.setAlignment(Pos.CENTER);
+    }
 }
