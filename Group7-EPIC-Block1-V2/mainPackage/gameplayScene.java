ebc6c3439de27d734dcbb46c33f515fe62cbd6d7
unknown <23371323@studentmail.ul.ie>
2023-11-29 10:36:30 +0000

    gameplayScene Functions and GUI improve

--- Group7-EPIC-Block1-V2/mainPackage/gameplayScene.java
+++ Group7-EPIC-Block1-V2/mainPackage/gameplayScene.java
@@ -18,35 +18,22 @@ import javafx.stage.Popup;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.TextAlignment;
 import javafx.util.Duration;
 
 public class gameplayScene extends Scene {
-	 Background transparentBackground = new Background(new BackgroundFill(Color.TRANSPARENT, CornerRadii.EMPTY, javafx.geometry.Insets.EMPTY));
-	 Background orangeBackground = new Background(new BackgroundFill(Color.rgb(232, 123, 56), CornerRadii.EMPTY, javafx.geometry.Insets.EMPTY));
-	 DropShadow dropShadow = new DropShadow();
-	
+    Background transparentBackground = new Background(new BackgroundFill(Color.TRANSPARENT, CornerRadii.EMPTY, javafx.geometry.Insets.EMPTY));
+    Background orangeBackground = new Background(new BackgroundFill(Color.rgb(232, 123, 56), CornerRadii.EMPTY, javafx.geometry.Insets.EMPTY));
+    DropShadow dropShadow = new DropShadow();
     static int gamemodeInt;
     Label Answer1 = new Label("Ans1");
-    
     Label Answer2 = new Label("Ans2");
-    
     Label Answer3 = new Label("Ans3");
-        
     Label Answer4 = new Label("Ans4");
-    
     Popup incorrectP = new Popup();
-    
     Label incorrectPLabel = new Label("Incorrect");
-    
     Popup correctP = new Popup();
     Label correctPLabel = new Label("Correct");
-    
-    
     Insets offset = new Insets(10,10,10,10);
-
     CheckBox Answer1ckBox = new CheckBox();
     CheckBox Answer2ckBox = new CheckBox();
     CheckBox Answer3ckBox = new CheckBox();
@@ -55,97 +42,75 @@ public class gameplayScene extends Scene {
     static int wins;
     static int losses;
     static int i = 0;
-
     Label currentScoreLabel = new Label("Current Score:" + score);
-    
-    
     Button nextButton = new Button("Next");
-    
-//****
     card[] cards_array;
+    Label questionLabel = new Label(cards_array[i].Question_content);
+    HBox answer1 = new HBox(Answer1ckBox, Answer1);
+    HBox answer2 = new HBox(Answer2ckBox, Answer2);
+    HBox answer3 = new HBox(Answer3ckBox, Answer3);
+    HBox answer4 = new HBox(Answer4ckBox, Answer4);
 
     public gameplayScene(Stage primaryStage) {
         super(new VBox(), 440, 350);
 
-        Answer1.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 20));
-        Answer1.setTextFill(Color.WHITE);
-        Answer1.setBackground(transparentBackground);
-        Answer1.setEffect(dropShadow);
-       
-        
-        Answer2.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 20));
-        Answer2.setTextFill(Color.WHITE);
-        Answer2.setBackground(transparentBackground);
-        Answer2.setEffect(dropShadow);
-        
-        
-        Answer3.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 20));
-        Answer3.setTextFill(Color.WHITE);
-        Answer3.setBackground(transparentBackground);
-        Answer3.setEffect(dropShadow);
-     
+        homeScene.labelCosmetics(Answer1, 20, transparentBackground, dropShadow);
 
-        Answer4.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 20));
-        Answer4.setTextFill(Color.WHITE);
-        Answer4.setBackground(transparentBackground);
-        Answer4.setEffect(dropShadow);
-       
-        
-        incorrectPLabel.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 20));
-        incorrectPLabel.setTextFill(Color.WHITE);
-        incorrectPLabel.setBackground(orangeBackground);
-        incorrectPLabel.setEffect(dropShadow);
-        incorrectPLabel.setTextAlignment(TextAlignment.CENTER);
-        
-        
-        currentScoreLabel.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 20));
-        currentScoreLabel.setTextFill(Color.WHITE);
-        currentScoreLabel.setBackground(transparentBackground);
-        currentScoreLabel.setEffect(dropShadow);
-        currentScoreLabel.setTextAlignment(TextAlignment.CENTER);
-       
-        
-        correctPLabel.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 20));
-        correctPLabel.setTextFill(Color.WHITE);
-        correctPLabel.setBackground(orangeBackground);
-        correctPLabel.setEffect(dropShadow);
-        correctPLabel.setTextAlignment(TextAlignment.CENTER);
-        
-        
-        nextButton.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 20));
-        nextButton.setTextFill(Color.WHITE);
-        nextButton.setBackground(orangeBackground);
-        nextButton.setEffect(dropShadow);
-        nextButton.setTextAlignment(TextAlignment.CENTER);
-        
-        
+        homeScene.labelCosmetics(Answer2, 20, transparentBackground, dropShadow);
+
+        homeScene.labelCosmetics(Answer3, 20, transparentBackground, dropShadow);
+
+        homeScene.labelCosmetics(Answer4, 20, transparentBackground, dropShadow);
+
+        homeScene.labelCosmetics(incorrectPLabel, 20, orangeBackground, dropShadow);
+        incorrectP.getContent().add(incorrectPLabel);
+
+        homeScene.labelCosmetics(currentScoreLabel, 20, transparentBackground, dropShadow);
+
+        homeScene.labelCosmetics(correctPLabel, 20, orangeBackground, dropShadow);
+        correctP.getContent().add(correctPLabel);
+
+        homeScene.buttonCosmetics(nextButton, orangeBackground, dropShadow, 20);
+
+        homeScene.labelCosmetics(questionLabel, 20, orangeBackground, dropShadow);
+
+        setGamemode();
+
+        setAnswers(questionLabel);
+        nextButtonAction(primaryStage, questionLabel);
+
+    VBox root = (VBox) this.getRoot();
+    root.getChildren().addAll(layoutMaker());
+    
+}
+    public static gameplayScene createScene(Stage primaryStage) {
+        return new gameplayScene(primaryStage);
+    }
+
+    public void setGamemode(){
         if (gamemodeInt == 0) {
             // difficulty
             cards_array = DB_CardInteract.allCardsIncreasingDifficulty();
         }
         else if (gamemodeInt == 1) {
-//score
+            //score
             cards_array = DB_CardInteract.allCardsIncreasingScore(launcher.user_ID);
         }
         else if (gamemodeInt == 2) {
+            //random
             cards_array = DB_CardInteract.returnAllCards();
             shuffle_array(cards_array);
         }
         else {
             System.out.println("Error: gamemodeInt not set");
         }
-//random
-        Label questionLabel = new Label(cards_array[i].Question_content);
-        questionLabel.setFont(Font.font("ADLam Display", FontWeight.NORMAL, 20));
-        questionLabel.setTextFill(Color.WHITE);
-        questionLabel.setBackground(orangeBackground);
-        questionLabel.setEffect(dropShadow);
-        questionLabel.setTextAlignment(TextAlignment.CENTER);
-        
+    }
+
+    public void nextButtonAction(Stage primaryStage, Label questionLabel ){
         nextButton.setOnAction(e -> {
-        	Answer3ckBox.setVisible(true);
+            Answer3ckBox.setVisible(true);
             Answer4ckBox.setVisible(true);
-        	if (AnswerSelection(Answer1ckBox, Answer2ckBox, Answer3ckBox, Answer4ckBox) == cards_array[i].Question_correct_answer){
+            if (AnswerSelection(Answer1ckBox, Answer2ckBox, Answer3ckBox, Answer4ckBox) == cards_array[i].Question_correct_answer){
                 // if correct, add 1 to score, and add win to scores_db and change score_db by 1
                 DB_ScoreInteract.addWin(launcher.user_ID, cards_array[i].card_id);
                 score += 1; // local score += 1
@@ -158,78 +123,48 @@ public class gameplayScene extends Scene {
                 losses += 1; // local losses += 1
                 IncorrectPopUp(incorrectP, primaryStage);
             }
+
             if (i < cards_array.length - 1) {
                 System.out.println("i = " + i);
                 System.out.println("cards_array.length = " + cards_array.length);
                 i++;
-            }
-            else {
+            } else {
                 DB_PlayHistory.addHistory(launcher.user_ID, score, wins, losses);
-
                 Stage stage = (Stage) nextButton.getScene().getWindow();
-                primaryStage.setScene(postgameScene.createScene(primaryStage, score));
+                stage.setScene(postgameScene.createScene(primaryStage, score));
                 primaryStage.setFullScreen(true);
                 resetScore();
-            };
-
-            System.out.println(score);
-
-            questionLabel.setText(cards_array[i].Question_content);
-            currentScoreLabel.setText("Current Score:" + score);
-            //** if array length
-            if (cards_array[i].Question_answers_arr.length == 4) {
-            	Answer1.setText(cards_array[i].Question_answers_arr[0]);
-            	Answer2.setText(cards_array[i].Question_answers_arr[1]);
-            	Answer3.setText(cards_array[i].Question_answers_arr[2]);
-            	Answer4.setText(cards_array[i].Question_answers_arr[3]);
-            	
-            	
-            }else if (cards_array[i].Question_answers_arr.length == 3) {
-            	Answer1.setText(cards_array[i].Question_answers_arr[0]);
-            	Answer2.setText(cards_array[i].Question_answers_arr[1]);
-            	Answer3.setText(cards_array[i].Question_answers_arr[2]);
-            	Answer4.setText(" ");
-            	Answer4ckBox.setVisible(false);
-        	}else{
-            	Answer1.setText(cards_array[i].Question_answers_arr[0]);
-                Answer2.setText(cards_array[i].Question_answers_arr[1]);
-                Answer3.setText(" ");                
-                Answer4.setText(" ");                
-                Answer3ckBox.setVisible(false);
-                Answer4ckBox.setVisible(false);
             }
+            System.out.println(score);
+            setAnswers(questionLabel);
         });
+    }
 
-    Answer1.setText(cards_array[i].Question_answers_arr[0]);
-    Answer2.setText(cards_array[i].Question_answers_arr[1]);
-    Answer3.setText(cards_array[i].Question_answers_arr[2]);
-    Answer4.setText(cards_array[i].Question_answers_arr[3]);
-    HBox answer1 = new HBox(Answer1ckBox, Answer1);
-    HBox answer2 = new HBox(Answer2ckBox, Answer2);
-    HBox answer3 = new HBox(Answer3ckBox, Answer3);
-    HBox answer4 = new HBox(Answer4ckBox, Answer4);
-    correctP.getContent().add(correctPLabel);
-    incorrectP.getContent().add(incorrectPLabel);
+    public void setAnswers(Label questionLabel){
+        questionLabel.setText(cards_array[i].Question_content);
+        currentScoreLabel.setText("Current Score:" + score);
+        //** if array length
+        if (cards_array[i].Question_answers_arr.length == 4) {
+            Answer1.setText(cards_array[i].Question_answers_arr[0]);
+            Answer2.setText(cards_array[i].Question_answers_arr[1]);
+            Answer3.setText(cards_array[i].Question_answers_arr[2]);
+            Answer4.setText(cards_array[i].Question_answers_arr[3]);
 
 
-    VBox CenterGameLay = new VBox();
-    CenterGameLay.setAlignment(Pos.CENTER);
-    CenterGameLay.getChildren().addAll(questionLabel, answer1, answer2, answer3, answer4 , nextButton);
-
-    BorderPane GameLay = new BorderPane();
-    GameLay.setStyle("-fx-background-color: #FFD966;");
-    GameLay.setPadding(offset);
-    GameLay.setBottom(currentScoreLabel);
-    GameLay.setCenter(CenterGameLay);
-
-    VBox root = (VBox) this.getRoot();
-    root.getChildren().addAll(GameLay);
-    
-}
-    public static gameplayScene createScene(Stage primaryStage) {
-    	
-        
-        return new gameplayScene(primaryStage);
+        }else if (cards_array[i].Question_answers_arr.length == 3) {
+            Answer1.setText(cards_array[i].Question_answers_arr[0]);
+            Answer2.setText(cards_array[i].Question_answers_arr[1]);
+            Answer3.setText(cards_array[i].Question_answers_arr[2]);
+            Answer4.setText(" ");
+            Answer4ckBox.setVisible(false);
+        }else{
+            Answer1.setText(cards_array[i].Question_answers_arr[0]);
+            Answer2.setText(cards_array[i].Question_answers_arr[1]);
+            Answer3.setText(" ");
+            Answer4.setText(" ");
+            Answer3ckBox.setVisible(false);
+            Answer4ckBox.setVisible(false);
+        }
     }
 
     public static int AnswerSelection(CheckBox Answer1ckBox, CheckBox Answer2ckBox, CheckBox Answer3ckBox, CheckBox Answer4ckBox) {
@@ -257,6 +192,7 @@ public class gameplayScene extends Scene {
         }
         return 9;
     }
+
     public static void shuffle_array(card[] cards_array){ //https://www.digitalocean.com/community/tutorials/shuffle-array-java#2-shuffle-array-using-random-class
         // shuffle array
         for (int i = 0; i < cards_array.length; i++) {
@@ -296,6 +232,26 @@ public class gameplayScene extends Scene {
         wins = 0;
         losses = 0;
         i = 0;
+    }
+
+    public VBox centerLayout(){
+        VBox CenterGameLay = new VBox(10);
+        CenterGameLay.setAlignment(Pos.CENTER);
+        CenterGameLay.getChildren().addAll(questionLabel, answer1, answer2, answer3, answer4 , nextButton);
+        CenterGameLay.setPrefSize(1080,900);
+
+        return CenterGameLay;
+    }
+
+    public BorderPane layoutMaker(){
+        BorderPane GameLay = new BorderPane();
+        GameLay.setStyle("-fx-background-color: #FFD966;");
+        GameLay.setPadding(offset);
+        GameLay.setBottom(currentScoreLabel);
+        GameLay.setPrefSize(1080,900);
+        GameLay.setCenter(centerLayout());
+
+        return GameLay;
     }
 }
 
